fc1f26df828cf5da3923137622186bde483a9c15
Martin Ruppert <martin.ruppert@microchip.com>
2025-08-24 18:30:38 +0200

    Modify lan865x_build_second.sh to unzip dts file

--- lan865x_build_second.sh
+++ lan865x_build_second.sh
@@ -6,10 +6,15 @@
 ## Patch the Kernel Driver, the DTS and the linux config for LAN865X support
 ## zip -r lan865x_driver.zip mscc-brsdk-source-2024.09/output/mybuild/build/linux-custom/drivers/net/ethernet/microchip/
 unzip lan865x_driver.zip
-tar xfv lan865x_dts.tar.gz
+## zip lan865x_dts.zip mscc-brsdk-source-2024.09/output/mybuild/build/linux-custom/arch/arm/boot/dts/microchip/lan966x-pcb8291.dts
+unzip lan865x_dts.zip
+##
 tar xfv linux-custom-patch.tar.gz
+##
 tar xfv linux-kernel-config.tar.gz
+##
 tar xfv lan865x_file_overlay.tar.gz
+##
 tar xfv lan865x_InitScript.tar.gz
 
 cd ./mscc-brsdk-source-2024.09/output/mybuild
