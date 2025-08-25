b9de3feef44f0a531388e8e7e988f13f467eed48
Martin Ruppert <martin.ruppert@microchip.com>
2025-08-25 21:08:45 +0200

    Fix unzip command typo in build script

--- lan865x_build_second.sh
+++ lan865x_build_second.sh
@@ -13,7 +13,7 @@ unzip lan865x_linux_custom_config.zip
 ## zip -r lan865x_file_overlay.zip mscc-brsdk-source-2024.09/board/mscc/common/rootfs_overlay/
 unzip lan865x_file_overlay.zip
 ## zip -r lan865x_buildroot_config.zip mscc-brsdk-source-2024.09/output/mybuild/.config
-unzip zip lan865x_buildroot_config.zip
+unzip lan865x_buildroot_config.zip
 
 
 cd ./mscc-brsdk-source-2024.09/output/mybuild
